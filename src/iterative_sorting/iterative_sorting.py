7334ec5fc6b8d34b8651e5ad22728cc29d65f8fb
Anna Winther <anski.anna@gmail.com>
2020-02-17 13:53:57 +0000

    Bubble sort refactored

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -11,25 +11,36 @@ def selection_sort( arr ):
                 smallest_index = j
                 # print(smallest_index)
         # TO-DO: swap
-        temporary = arr[i]
+        temp = arr[i]
         arr[i] = arr[smallest_index]
-        arr[smallest_index] = temporary
+        arr[smallest_index] = temp
 
     return arr
 # print(selection_sort([5,2,1,9,0,4,6]))
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-    for array in range(len(arr) - 1, 0, -1):
-        for i in range(array):
-            print(i)
-            if arr[i] > arr[i+1]:
-                temp = arr[i]
-                print('k', temp)
-                arr[i] = arr[i+1]
-                print('l', arr[i])
-                arr[i+1] = temp
-                print('t', arr[i+1])
+    for i in range(len(arr)):
+        # last i are already in place 
+        for j in range(0, len(arr) - i - 1):
+            # if the element is found greater than the next element, swap them
+            if arr[j] > arr[j+1]:
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+                # without temp variabale:
+                # arr[j], arr[j+1] = arr[j+1], arr[j]
+    
+    # for array in range(len(arr) - 1, 0, -1):
+    #     for i in range(array):
+    #         print(i)
+    #         if arr[i] > arr[i+1]:
+    #             temp = arr[i]
+    #             print('k', temp)
+    #             arr[i] = arr[i+1]
+    #             print('l', arr[i])
+    #             arr[i+1] = temp
+    #             print('t', arr[i+1])
 
     return arr
 
