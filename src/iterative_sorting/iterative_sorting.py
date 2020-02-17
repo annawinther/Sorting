b17f987ede64f12d486b052c3af3b0ab85257adf
Anna Winther <anski.anna@gmail.com>
2020-02-17 14:00:29 +0000

    Fix test not passing in selective sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,22 +1,21 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(0, len(arr) -1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-        for j in range(i+1, len(arr) -1):
+        for j in range(i+1, len(arr)):
             if arr[smallest_index] > arr[j]:
                 smallest_index = j
-                # print(smallest_index)
         # TO-DO: swap
         temp = arr[i]
         arr[i] = arr[smallest_index]
         arr[smallest_index] = temp
 
     return arr
-# print(selection_sort([5,2,1,9,0,4,6]))
+print(selection_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
@@ -44,7 +43,7 @@ def bubble_sort( arr ):
 
     return arr
 
-print(bubble_sort([5,2,1,9,0,4,6]))
+print(bubble_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
