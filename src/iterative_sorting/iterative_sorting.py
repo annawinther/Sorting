f332f604febeaed3545eef11dff227bb7d450514
Anna Winther <anski.anna@gmail.com>
2020-02-18 09:13:22 +0000

    Started on the next task on Monday - the merge sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -17,7 +17,7 @@ def selection_sort( arr ):
     return arr
 print(selection_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
 
-# TO-DO:  implement the Bubble Sort function below
+# TO-DO: implement the Bubble Sort function below
 def bubble_sort( arr ):
     for i in range(len(arr)):
         # last i are already in place 
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,15 +1,35 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
+# TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    
+    # A helper function that handles merging sorted pieces back together
+   
+
+    # left_idx = 0
+    # right_idx = 0
+    # while len(arrA) > left_idx and len(arrB) > right_idx:
+    #     if arrA[left_idx] < arrB[right_idx]:
+    #         merged_arr.append(arrA[left_idx])
+    #         left_idx =+ 1
+    #     else: 
+    #         merged_arr.append(arrB[right_idx])
+    #         right_idx =+ 1
+    for i in range(elements):
+        if i < i + 1:
+            merged_arr.append(i)
+    # merged_arr += arrA[left_idx:]
+    # merged_arr =+ arrB[right_idx:]
+
     return merged_arr
 
-
+print(merge([1,4,2], [5,7,6]))
 # TO-DO: implement the Merge Sort function below USING RECURSION
+#  A recursive function that handles dividing the array (or subarray) in half
 def merge_sort( arr ):
     # TO-DO
+    if len( arr ) > 1:
+        pass
 
     return arr
 
